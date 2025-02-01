9730c3ca48da415441844a763d60ec81d246b507
MeowWow520 <128897166+MeowWow520@users.noreply.github.com>
2025-02-01 14:00:45 +0800

    Create Day250201.c

---
+++ Day250201.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+
+#include <stdio.h>
+
+
+int main() {
+
+	int arr[] = { 73, 32, 99, 97, 110, 32, 100, 111, 32, 105, 116 ,33 };
+	for (int i = 0; i <= 11; i++) {
+		printf("%c", arr[i]);
+	}
+
+	return 0;
+}
